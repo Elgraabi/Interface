b33d048af8b70b2d5f9a8e29c457eb3d40eb3721
Elgraabi <silvatdb8@gmail.com>
2024-02-16 20:15:28 -0300

    Interface

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,28 @@
+package model.entities;
+
+public class Vehicle {
+
+	// Atributos
+	private String model;
+
+	// Metodos Especiais
+
+	// Metodos Especificos
+	public Vehicle() {
+		super();
+	}
+
+	public Vehicle(String model) {
+		super();
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+}
---
+++ src/model/services/TaxService.java
@@ -0,0 +1,10 @@
+package model.services;
+
+public interface TaxService {
+	
+	// Atributos
+	double tax(double amount);
+	
+	// Metodos Especiais
+	// Metodos Especificos
+}
